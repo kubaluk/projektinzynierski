e24e46d908255bc1af67a6df2ea3229e712ab50f
Jakub Łukasik <kubaluk7573@o2.pl>
2021-10-17 12:55:45 +0200

    added comments to recent code

--- game/Assets/Scripts/KeyboardMovementController.cs
+++ game/Assets/Scripts/KeyboardMovementController.cs
@@ -1,7 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-
+//movement controller using keyboard
 public class KeyboardMovementController : MonoBehaviour
 {
     private void Update()
