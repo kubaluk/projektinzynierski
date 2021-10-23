b2496cc4ab458d3dadc52898d098182f065e344e
Jakub Łukasik <kubaluk7573@o2.pl>
2021-10-23 19:26:29 +0200

    Character swapping system

--- game/Assets/Scripts/PlayerController.cs
+++ game/Assets/Scripts/PlayerController.cs
@@ -1,18 +1,84 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UIElements;
 
 public class PlayerController : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    private enum PlayerState{Active, Follow, Swap};
+
+    [SerializeField] private PlayerState state;
+    private PlayerState previousState;
+    [SerializeField] private GameObject movePoint;
+    [SerializeField] private GameObject otherCharacter;
+    [SerializeField] private float swapTime;
+    [SerializeField] private GameObject playerSprite;
+    private Renderer spriteRenderer;
+    private float t;
+
+    private void OnSwapButton()
     {
-        
+        if (state != PlayerState.Swap)
+        {
+            previousState = state;
+            spriteRenderer.sortingOrder = (int) state;
+            state = PlayerState.Swap;
+            t = 0;
+        }
     }
 
-    // Update is called once per frame
-    void Update()
+    private void Swap()
     {
-        
+        t += Time.deltaTime;
+        if (t / swapTime < 1)
+        {
+            switch (previousState)
+            {
+                case PlayerState.Active:
+                    transform.position = movePoint.transform.position;
+                    break;
+                case PlayerState.Follow:
+                    transform.localPosition = Vector3.Lerp(transform.localPosition,movePoint.transform.localPosition, t/swapTime);
+                    break;
+            }
+        }
+        else
+        {
+            switch (previousState)
+            {
+                case PlayerState.Active:
+                    state = PlayerState.Follow;
+                    break;
+                case PlayerState.Follow:
+                    state = PlayerState.Active;
+                    break;
+            }
+        }
+    }
+    // Start is called before the first frame update
+    private void Start()
+    {
+        EventSystem.Current.ONSwapButtonPressed += OnSwapButton;
+        spriteRenderer = playerSprite.GetComponent<Renderer>();
+        t = 0;
+    }
+    
+    
+
+    // Update is called once per frame
+    private void Update()
+    {
+        switch (state)
+        {
+            case PlayerState.Active:
+                transform.position = movePoint.transform.position;
+                break;
+            case PlayerState.Follow:
+                //TODO: following script
+                break;
+            case PlayerState.Swap:
+                Swap();
+                break;
+        }
     }
 }
